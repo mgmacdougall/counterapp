1f6c990e5176e783f98e22de49e4a298ca1f72dd
Michael MacDougall <32442194+mgmacdougall@users.noreply.github.com>
2023-05-20 19:33:11 -0400

    Initial Commit
    Project initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let messageEl = document.getElementById("save-el");
+
+// Global vars
+let count = 0;
+let countCache = [];
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+  updateCache();
+}
+
+function updateCache() {
+  countCache.push(count);
+}
+
+function flushCache() {
+  countCache = [];
+}
+
+function updateMessage() {
+  const message = countCache.join(",").replaceAll(",", "-");
+  messageEl.innerText = message;
+}
+
+function clearMessage() {
+  messageEl.innerText = "--";
+}
+
+function resetCounter() {
+  count = 0;
+}
+
+function resetApp() {
+  countEl.textContent = 0;
+  resetCounter();
+  flushCache();
+  clearMessage();
+}
+
+function save() {
+  updateMessage();
+  resetCounter();
+  flushCache();
+  
+}
